8738994005a0c0d3a74cf74979310d9ada96db46
marcin <mniskiewicz@gmail.com>
2024-05-18 19:45:28 +0200

    changes

--- docker/storage-fastapi-backend/textGenerators/ChatHelpers.py
+++ docker/storage-fastapi-backend/textGenerators/ChatHelpers.py
@@ -32,6 +32,11 @@ def prepare_chat_history(chat_history, memory_token_limit, model_name):
 
 def num_tokens_from_string(string: str, model: str) -> int:
     """Returns the number of tokens in a text string."""
-    encoding = tiktoken.encoding_for_model(model)
+    try:
+      encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+      print("Warning: model not found. Using cl100k_base encoding.")
+      encoding = tiktoken.get_encoding("cl100k_base")
+  
     num_tokens = len(encoding.encode(string))
     return num_tokens
