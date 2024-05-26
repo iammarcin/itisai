744f512dd931a790ab9cead873b5524e5643a261
marcin <mniskiewicz@gmail.com>
2024-05-26 08:51:59 +0200

    changes

--- docker/storage-fastapi-backend/db/dbProvider.py
+++ docker/storage-fastapi-backend/db/dbProvider.py
@@ -65,12 +65,10 @@ class dbProvider:
       return await self.get_chat_session(userInput['session_id'], customerId)
     elif action == "db_new_message":
       return await self.create_chat_message(userInput)
-    '''
     elif action == "db_search_messages":
-        return self.search_chat_messages_for_user(userInput['customer_id'], userInput['search_text'])
+      return await self.search_chat_messages_for_user(userInput['customer_id'], userInput['search_text'])
     else:
         raise HTTPException(status_code=400, detail="Unknown action")
-    '''
 
   async def create_new_chat_session(self, userInput: dict, customerId: int):
     async with AsyncSessionLocal() as session:
@@ -89,8 +87,9 @@ class dbProvider:
   async def get_all_chat_sessions_for_user(self, customer_id: int):
     async with AsyncSessionLocal() as session:
       result = await session.execute(
-        select(ChatSession).where(ChatSession.customer_id == customer_id)
+        select(ChatSession).where(ChatSession.customer_id == customer_id).order_by(ChatSession.last_update.desc())
       )
+
       sessions = result.scalars().all()
       sessions_list = [self.to_dict(session) for session in sessions]
 
@@ -111,35 +110,50 @@ class dbProvider:
         return JSONResponse(content={"success": True, "code": 200, "message": {"status": "completed", "result": chat_session_content}}, status_code=200)
 
   async def create_chat_message(self, userInput: dict):
-      async with AsyncSessionLocal() as session:
-          async with session.begin():
-              if userInput['message'] == config.defaults['ERROR_MESSAGE_FOR_TEXT_GEN']:
-                 return JSONResponse(content={"success": False, "code": 400, "message": {"status": "completed", "result": 'Pb with text gen. not saving to DB'}}, status_code=200)
-              try:
-                # first create new item in chat message
-                new_message = ChatMessage(
-                    session_id=userInput['session_id'],
-                    customer_id=userInput['customer_id'],
-                    sender=userInput['sender'],
-                    message=userInput['message'],
-                    image_locations=userInput.get('image_locations'),
-                    file_locations=userInput.get('file_locations')
-                )
-                session.add(new_message)
+    async with AsyncSessionLocal() as session:
+      async with session.begin():
+        if userInput['message'] == config.defaults['ERROR_MESSAGE_FOR_TEXT_GEN']:
+          return JSONResponse(content={"success": False, "code": 400, "message": {"status": "completed", "result": 'Pb with text gen. not saving to DB'}}, status_code=200)
+        try:
+          # first create new item in chat message
+          new_message = ChatMessage(
+            session_id=userInput['session_id'],
+            customer_id=userInput['customer_id'],
+            sender=userInput['sender'],
+            message=userInput['message'],
+            image_locations=userInput.get('image_locations'),
+            file_locations=userInput.get('file_locations')
+          )
+          session.add(new_message)
 
-                # Update chat session's chat_history and last_update
-                chat_session = await session.get(ChatSession, userInput['session_id'])
-                if chat_session:
-                    # Use chat_history from userInput directly
-                    chat_session.chat_history = json.dumps(userInput['chat_history'])
-                    chat_session.last_update = func.now()
-                else:
-                    raise HTTPException(status_code=404, detail="Chat session not found")
+          # Update chat session's chat_history and last_update
+          chat_session = await session.get(ChatSession, userInput['session_id'])
+          if chat_session:
+            # Use chat_history from userInput directly
+            chat_session.chat_history = json.dumps(userInput['chat_history'])
+            chat_session.last_update = func.now()
+          else:
+            raise HTTPException(status_code=404, detail="Chat session not found")
 
-                result = await session.commit()
-                logger.info("Result of commit: %s", result)
+          result = await session.commit()
+          logger.info("Result of commit: %s", result)
 
-                return JSONResponse(content={"success": True, "code": 200, "message": {"status": "completed", "result": "New message recorded"}}, status_code=200)
-              except Exception as e:
-                logger.error("Error in create_chat_message: %s", str(e))
-                return JSONResponse(content={"False": True, "code": 400, "message": {"status": "fail", "result": str(e)}}, status_code=400)
+          return JSONResponse(content={"success": True, "code": 200, "message": {"status": "completed", "result": "New message recorded"}}, status_code=200)
+        except Exception as e:
+          logger.error("Error in create_chat_message: %s", str(e))
+          return JSONResponse(content={"False": True, "code": 400, "message": {"status": "fail", "result": str(e)}}, status_code=400)
+
+  async def search_chat_messages_for_user(self, customer_id: int, search_text: str):
+    async with AsyncSessionLocal() as session:
+      async with session.begin():
+        stmt = select(ChatMessage).join(ChatSession).where(
+          ChatMessage.customer_id == customer_id,
+          ChatMessage.message.ilike(f"%{search_text}%")
+        ).order_by(ChatMessage.created_at.desc())
+        result = await session.execute(stmt)
+        messages = result.scalars().all()
+        sessions_list = [self.to_dict(message)['session_id'] for message in messages] 
+
+        logger.info("All sessions with search message for user %s: %s", customer_id, sessions_list)
+        #logger.info("All session ids for user %s: %s", customer_id, session_ids)
+        return JSONResponse(content={"success": True, "code": 200, "message": {"status": "completed", "result": sessions_list}}, status_code=200)
