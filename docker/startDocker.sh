8477c7d2850a363faed4950421f8ecfcc7e5624f
marcin <mniskiewicz@gmail.com>
2024-05-28 19:53:31 +0200

    changes

--- docker/startDocker.sh
+++ docker/startDocker.sh
@@ -6,6 +6,12 @@ if [ $HOSTNAME == 'sherlock' ]; then
   export NODE_ENV=sherlock
   docker-compose -f docker-compose.yml --env-file .env.sherlock down
   docker-compose -f docker-compose.yml --env-file .env.sherlock up
+elif [ $HOSTNAME == 'ip-172-31-8-221' ]; then
+  . /home/ubuntu/.profile
+  echo atamaibiz
+  export NODE_ENV=production
+  docker-compose -f docker-compose.yml --env-file .env.atamaibiz down
+  docker-compose -f docker-compose.yml --env-file .env.atamaibiz up >> /c/docker/storage-common/testApi/logs/docker.sh.log 2>&1
 else
   source ~/.bash_profile
   echo local
