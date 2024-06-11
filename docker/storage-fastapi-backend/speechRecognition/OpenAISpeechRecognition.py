003ab8a77aa8d931d77e2cd8a7514fa299a0342e
mn <mn@mn>
2024-06-11 11:15:01 +0200

    changes

--- docker/storage-fastapi-backend/speechRecognition/OpenAISpeechRecognition.py
+++ docker/storage-fastapi-backend/speechRecognition/OpenAISpeechRecognition.py
@@ -1,107 +1,119 @@
+from helperUploadDownload import saveContentToFile
+from openai import OpenAI
 from fastapi import HTTPException
 from fastapi.responses import JSONResponse
 
 import logconfig
 import traceback
-import re, json, os
+import re
+import json
+import os
 logger = logconfig.logger
-from openai import OpenAI
-from helperUploadDownload import saveContentToFile
+
 
 class OpenAISpeechRecognitionGenerator:
-  def __init__(self):
-    self.save_to_file = True
-    self.save_to_file_iterator = 0
-    self.model_name = "whisper-1"
-    self.optional_prompt = ""
-    # between 0 and 1, higher - more random, 0 - log prob increasing temperature automatically
-    self.temperature = 0.2
-    # vtt - with timestamps, i think text should be set as standard so later in save2file we have no problem
-    self.response_format = "text"
-    self.language = "en"
-    self.client = OpenAI()
+    def __init__(self):
+        self.save_to_file = True
+        self.save_to_file_iterator = 0
+        self.model_name = "whisper-1"
+        self.optional_prompt = ""
+        # between 0 and 1, higher - more random, 0 - log prob increasing temperature automatically
+        self.temperature = 0.2
+        # vtt - with timestamps, i think text should be set as standard so later in save2file we have no problem
+        self.response_format = "text"
+        self.language = "en"
+        self.client = OpenAI()
 
-  def set_settings(self, user_settings={}):
-    if user_settings:
-        # if we want to return test data
-        self.use_test_data = user_settings["general"].get("returnTestData", False)
+    def set_settings(self, user_settings={}):
+        if user_settings:
+            # if we want to return test data
+            self.use_test_data = user_settings["general"].get(
+                "returnTestData", False)
 
-        # and now process speech settings
-        user_settings = user_settings.get("speech", {})
-        logger.debug("Setting user_settings: %s", user_settings)
-        if "response_format" in user_settings:
-            self.response_format = user_settings["response_format"]
+            # and now process speech settings
+            user_settings = user_settings.get("speech", {})
+            logger.debug("Setting user_settings: %s", user_settings)
+            if "response_format" in user_settings:
+                self.response_format = user_settings["response_format"]
 
-        if "temperature" in user_settings:
-            self.temperature = user_settings["temperature"]
+            if "temperature" in user_settings:
+                self.temperature = user_settings["temperature"]
 
-        if "language" in user_settings:
-            self.language = user_settings["language"]
+            if "language" in user_settings:
+                self.language = user_settings["language"]
 
-        if "model" in user_settings:
-            self.model_name = user_settings["model"]
+            if "model" in user_settings:
+                self.model_name = user_settings["model"]
 
-        if "optional_prompt" in user_settings:
-            self.optional_prompt = user_settings["optional_prompt"]
+            if "optional_prompt" in user_settings:
+                self.optional_prompt = user_settings["optional_prompt"]
 
-  async def process_job_request(self, action: str, userInput: dict, assetInput: dict, customerId: int = None, userSettings: dict = {}):
-    # OPTIONS
-    self.set_settings(userSettings)
-    try:
-      if action == "transcribe" or action == "translate" or action == "chat":
-        return await self.whisper(action,userInput, assetInput, customerId, userSettings)
-      else:
-        raise HTTPException(status_code=400, detail="Unknown action")
-    except Exception as e:
-      logger.error("Error processing speech request: %s", str(e))
-      raise HTTPException(status_code=500, detail="Error processing speech request")
+    async def process_job_request(self, action: str, userInput: dict, assetInput: dict, customerId: int = None, userSettings: dict = {}):
+        # OPTIONS
+        self.set_settings(userSettings)
+        try:
+            if action == "transcribe" or action == "translate" or action == "chat":
+                return await self.whisper(action, userInput, assetInput, customerId, userSettings)
+            else:
+                raise HTTPException(status_code=400, detail="Unknown action")
+        except Exception as e:
+            logger.error("Error processing speech request: %s", str(e))
+            raise HTTPException(
+                status_code=500, detail="Error processing speech request")
 
-  async def whisper(self, action: str, userInput: dict, assetInput: dict, customerId: int = None, userSettings: dict = {}):
-    logger.debug("OpenAISpeechGenerator whisper - start")
+    async def whisper(self, action: str, userInput: dict, assetInput: dict, customerId: int = None, userSettings: dict = {}):
+        logger.debug("OpenAISpeechGenerator whisper - start")
 
-    if self.use_test_data:
-        return {'code': 200, 'success': True, 'message': {"status": "completed", "result": "Hello! (TEST transcribed)"}}
+        if self.use_test_data:
+            return {'code': 200, 'success': True, 'message': {"status": "completed", "result": "Hello! (TEST transcribed)"}}
 
-    try:
-      # only for chat - we have to save blob to file (as its coming from recorder from Chat in react)
-      if action == "chat":
-        upload_file = userInput.get('file')
-        audio_file = await saveContentToFile(customerId, upload_file, 1)
-        file = audio_file['message']
-        audio_file = open(file, "rb")
-      else: # for rest we have file that we need to feed to whisper
-        # TEMP TEST
-        audio_file = open("/storage/testApi/20230419_391sa2_output_1.mp3", "rb")
+        try:
+            # only for chat - we have to save blob to file (as its coming from recorder from Chat in react)
+            if action == "chat":
+                upload_file = userInput.get('file')
+                audio_file = await saveContentToFile(customerId, upload_file, 1)
+                file = audio_file['message']
+                audio_file = open(file, "rb")
+            else:  # for rest we have file that we need to feed to whisper
+                # TEMP TEST
+                audio_file = open(
+                    "/storage/testApi/20230419_391sa2_output_1.mp3", "rb")
 
-      if action == "translate":
-        response = self.client.audio.translations.create(
-            model=self.model_name,
-            file=audio_file,
-            prompt=self.optional_prompt,
-            response_format=self.response_format,
-            temperature=self.temperature)
-      else:
-        response = self.client.audio.transcriptions.create(
-          model=self.model_name,
-          file=audio_file,
-          prompt=self.optional_prompt,
-          response_format=self.response_format,
-          temperature=self.temperature,
-          language=self.language)
+            if action == "translate":
+                response = self.client.audio.translations.create(
+                    model=self.model_name,
+                    file=audio_file,
+                    prompt=self.optional_prompt,
+                    response_format=self.response_format,
+                    temperature=self.temperature)
+            else:
+                response = self.client.audio.transcriptions.create(
+                    model=self.model_name,
+                    file=audio_file,
+                    prompt=self.optional_prompt,
+                    response_format=self.response_format,
+                    temperature=self.temperature,
+                    language=self.language)
 
-      response_text = response.text if hasattr(response, 'text') else response
+            response_text = response.text if hasattr(
+                response, 'text') else response
 
-      logger.info("OpenAISpeechGenerator whisper - response: %s" % response_text)
-      logger.debug("OpenAISpeechGenerator whisper - success")
+            logger.info("OpenAISpeechGenerator whisper - response: %s" %
+                        response_text)
+            logger.debug("OpenAISpeechGenerator whisper - success")
 
-      return JSONResponse(content={"success": True, "code": 200, "message": {"status": "completed", "result": response_text}}, status_code=200)
+            return JSONResponse(content={"success": True, "code": 200, "message": {"status": "completed", "result": response_text}}, status_code=200)
 
-    except HTTPException as e:
-        logger.error("Error while making speech API call to OpenAI - HTTPException ")
-        logger.error(e)
-        raise HTTPException(status_code=500, detail="Error generating speech")
-    except Exception as e:
-        logger.error("Error while making speech API call to OpenAI - exception ")
-        logger.error(e)
-        traceback.print_exc()
-        raise HTTPException(status_code=500, detail="Error generating speech")
+        except HTTPException as e:
+            logger.error(
+                "Error while making speech API call to OpenAI - HTTPException ")
+            logger.error(e)
+            raise HTTPException(
+                status_code=500, detail="Error generating speech")
+        except Exception as e:
+            logger.error(
+                "Error while making speech API call to OpenAI - exception ")
+            logger.error(e)
+            traceback.print_exc()
+            raise HTTPException(
+                status_code=500, detail="Error generating speech")
--- docker/storage-fastapi-backend/textGenerators/ChatHelpers.py
+++ docker/storage-fastapi-backend/textGenerators/ChatHelpers.py
@@ -5,6 +5,8 @@ logger = logconfig.logger
 # image_message_limit - how many user messages can be sent after an image message before we start trimming image URLs
 # goal is to feed openai API with image URLs only when necessary (for few messages, later for sure topic will change)
 # support_image_input some model support images as input, some not
+
+
 def prepare_chat_history(chat_history, memory_token_limit, model_name, support_image_input, image_message_limit=3):
     total_tokens = 0
     trimmed_messages = []
@@ -22,11 +24,12 @@ def prepare_chat_history(chat_history, memory_token_limit, model_name, support_i
             for i, content_item in enumerate(message["content"]):
                 if content_item["type"] == "text":
                     # sometimes message can empty and then it looks like :
-                    #{'role': 'user', 'content': [{'type': 'text', 'text': ''}]}
+                    # {'role': 'user', 'content': [{'type': 'text', 'text': ''}]}
                     # in this case we should skip it
                     if content_item["text"] == "":
                         continue
-                    message_tokens = num_tokens_from_string(content_item["text"], model=model_name)
+                    message_tokens = num_tokens_from_string(
+                        content_item["text"], model=model_name)
                     text_content_index = i
                     break
 
@@ -49,14 +52,17 @@ def prepare_chat_history(chat_history, memory_token_limit, model_name, support_i
                 # and we would need to encode and decode exactly message to get it precisely
                 # so i decided that more or less is enough - and i will just cut % of the message (knowing how many tokens i have to trim to fit into limit)
                 if message_tokens > 0:  # Avoid division by zero
-                    trim_percentage = (message_tokens - remaining_tokens) / message_tokens
-                    trim_index = int(len(message["content"][text_content_index]["text"]) * trim_percentage)
+                    trim_percentage = (
+                        message_tokens - remaining_tokens) / message_tokens
+                    trim_index = int(
+                        len(message["content"][text_content_index]["text"]) * trim_percentage)
                     message["content"][text_content_index]["text"] = message["content"][text_content_index]["text"][trim_index:]
 
                 if include_image_urls:
                     trimmed_messages.insert(0, message)
                 else:
-                    message["content"] = [content_item for content_item in message["content"] if content_item["type"] != "image_url"]
+                    message["content"] = [
+                        content_item for content_item in message["content"] if content_item["type"] != "image_url"]
                     trimmed_messages.insert(0, message)
                 break
             else:
@@ -64,18 +70,21 @@ def prepare_chat_history(chat_history, memory_token_limit, model_name, support_i
                 if include_image_urls:
                     trimmed_messages.insert(0, message)
                 else:
-                    message["content"] = [content_item for content_item in message["content"] if content_item["type"] != "image_url"]
+                    message["content"] = [
+                        content_item for content_item in message["content"] if content_item["type"] != "image_url"]
                     trimmed_messages.insert(0, message)
         else:
             # For assistant messages which are not lists
-            message_tokens = num_tokens_from_string(message["content"], model=model_name)
+            message_tokens = num_tokens_from_string(
+                message["content"], model=model_name)
 
             if total_tokens + message_tokens > memory_token_limit:
                 remaining_tokens = memory_token_limit - total_tokens
 
                 # trick with percentage - described above
                 if message_tokens > 0:  # Avoid division by zero
-                    trim_percentage = (message_tokens - remaining_tokens) / message_tokens
+                    trim_percentage = (
+                        message_tokens - remaining_tokens) / message_tokens
                     trim_index = int(len(message["content"]) * trim_percentage)
                     message["content"] = message["content"][trim_index:]
 
@@ -87,13 +96,14 @@ def prepare_chat_history(chat_history, memory_token_limit, model_name, support_i
 
     return trimmed_messages
 
+
 def num_tokens_from_string(string: str, model: str) -> int:
     """Returns the number of tokens in a text string."""
     try:
-      encoding = tiktoken.encoding_for_model(model)
+        encoding = tiktoken.encoding_for_model(model)
     except KeyError:
-      print("Warning: model not found. Using cl100k_base encoding.")
-      encoding = tiktoken.get_encoding("cl100k_base")
-  
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+
     num_tokens = len(encoding.encode(string))
     return num_tokens
