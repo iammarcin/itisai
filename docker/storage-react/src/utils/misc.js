3ee9f4cea41aa8cb56a28861a7b9715fc3483c1a
Marcin <mniskiewicz@gmail.com>
2024-06-18 16:50:08 +0200

    changes

--- docker/storage-react/src/utils/misc.js
+++ docker/storage-react/src/utils/misc.js
@@ -10,34 +10,4 @@ const formatDate = (dateString) => {
  return `${year}-${month}-${day} ${hours}:${minutes}`;
 };
 
-/*
-const scrollToBottom = (option) => {
- const isAtBottom = Math.ceil(window.innerHeight + window.scrollY + 50) >= document.documentElement.scrollHeight;
-
- if (isAtBottom) {
-  if (!isMobile && option === "streaming") {
-   const botTextAreaContainer = document.querySelector('.botTextAreaContainer');
-
-   if (messagesEndRef.current && botTextAreaContainer) {
-    // Calculate the position just above the botTextAreaContainer
-    const scrollTargetPosition =
-     botTextAreaContainer.getBoundingClientRect().top - window.innerHeight + botTextAreaContainer.offsetHeight;
-
-    // Scroll smoothly to the target position
-    window.scrollBy({
-     top: scrollTargetPosition,
-     behavior: 'smooth',
-    });
-   }
-  } else {
-   if (messagesEndRef.current) {
-    messagesEndRef.current?.scrollIntoView({
-     behavior: "smooth",
-    });
-   }
-  }
- }
-
-};*/
-
 export { formatDate };
