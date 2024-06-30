c1cc8c263e7a8abedddb754eb380623a6982c916
Marcin <mniskiewicz@gmail.com>
2024-06-30 08:17:02 +0200

    changes

--- docker/storage-react/src/components/ChatWindow.js
+++ docker/storage-react/src/components/ChatWindow.js
@@ -68,15 +68,18 @@ const ChatWindow = ({ chatContent, setChatContent, currentSessionIndex, currentS
   // for AI response is simple - because its just last message in chat content
   // but for user request - we need to check little bit more
   const isLastMessage = (index, message) => {
+    console.log("chatContent 345: ", chatContent)
+    console.log("chatContent.length: ", chatContent.length, " index: ", index, " message: ", message.message)
     if (!message) return false;
+    const currentChatContent = chatContent[currentSessionIndex].messages;
 
     if (message.isUserMessage) {
       // Check if the next message exists and is an AI response
-      return (index === chatContent.length - 1) ||
-        (index === chatContent.length - 2 && !chatContent[index + 1].isUserMessage);
+      return (index === currentChatContent.length - 1) ||
+        (index === currentChatContent.length - 2 && !currentChatContent[index + 1].isUserMessage);
     } else {
       // For AI messages, the original logic works
-      return index === chatContent.length - 1;
+      return index === currentChatContent.length - 1;
     }
   };
 
