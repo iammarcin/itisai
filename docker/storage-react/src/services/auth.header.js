2ebbfbdae303d26ce74b50fa3b155588cff5a58a
Marcin <mniskiewicz@gmail.com>
2024-06-30 03:57:29 +0200

    changes

--- docker/storage-react/src/services/auth.header.js
+++ docker/storage-react/src/services/auth.header.js
@@ -1,3 +1,7 @@
+import {
+  getAuthTokenForBackend,
+} from '../utils/configuration';
+
 export default function authHeader() {
   const user = 1; // JSON.parse(localStorage.getItem('user'));
   if (user) { // && user.value.accessToken) {
@@ -5,9 +9,11 @@ export default function authHeader() {
     //return { 'x-access-token': user.value.accessToken };
     // for fastapi
     // get token
-    const authToken = JSON.parse(localStorage.getItem('authToken'));
+    //const authToken2 = JSON.parse(localStorage.getItem('authToken'));
+    //return { Authorization: authToken && authToken.token ? 'Bearer ' + authToken.token : '' };
+    const authToken = getAuthTokenForBackend();
 
-    return { Authorization: authToken && authToken.token ? 'Bearer ' + authToken.token : '' };
+    return { Authorization: authToken ? 'Bearer ' + authToken : '' };
   } else {
     return {};
   }
