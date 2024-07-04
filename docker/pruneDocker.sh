24f6a1c6a3eb4f54ecb25e0527f8791c6e62ed06
marcinn <mn@mn>
2024-07-04 07:02:00 +0000

    changes

--- docker/pruneDocker.sh
+++ docker/pruneDocker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker system prune -f #--volumes #--all
+docker system prune -f 
+docker system prune -f --all
+docker system prune -f --volumes --all
+docker rm -vf $(docker ps -aq)
+docker rmi -f $(docker images -aq)
+docker volume prune -f --all
